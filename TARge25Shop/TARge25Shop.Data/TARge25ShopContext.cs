48883911f8cba6cc5f4bdb846fe33de720b26f0a
henrykuningas5512 <henry.kuningas5512@gmail.com>
2026-09-07 15:26:54 +0300

    Vigane programm

---
+++ TARge25Shop/TARge25Shop.Data/TARge25ShopContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TARge25Shop.Data
+{
+    public class TARge25ShopContext : DbContext
+    {
+        
+        //see class tuleb teha DbContextiks,
+        //et saaks kasutada Entity Frameworki andmebaasi operatsioone
+    }
+}
