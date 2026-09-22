939885af813f10a7a3493eab098c445cecc5ad28
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-22 19:29:38 +0300

    Oih unustasin saveida

--- TARge25Shop/TARge25Shop.SpaceshipTest/SpaceshipTest.cs
+++ TARge25Shop/TARge25Shop.SpaceshipTest/SpaceshipTest.cs
@@ -43,5 +43,47 @@ namespace TARge25Shop.SpaceshipTest
             //kontroll
             Assert.NotEqual(wrongGuid, goodGuid);
         }
+        [Fact]
+        public async Task Should_GetSpaceshipByID_WhenGuidIsEqual()
+        {
+            //ülessezade
+            Guid databaseGuid = Guid.Parse("9e9205b2-2637-4156-bb15-900f5934b5e9");
+            Guid seekGuid = Guid.Parse("9e9205b2-2637-4156-bb15-900f5934b5e9");
+
+
+            //tegevus
+            await Svc<ISpaceshipServices>().DetailAsync(seekGuid);
+
+            //kontroll
+            Assert.Equal(databaseGuid, seekGuid);
+        }
+        //seleta kodus lahti, nagu eelnevate testide laused, eesti keelde, selle testi óma ka.----- siit edasi on midagi puudu :) hehe
+
+        [Fact]
+        public async Task Should_SpaceshipDeletedByID_WhenReturnedResultIsEqual()
+        {
+            //ülesseade
+            SpaceshipDto dto = MockSpaceshipData();
+
+            var addSpaceship = await Svc<ISpaceshipServices>().Create(dto);
+            var deleteSpaceship = await Svc<ISpaceshipServices>().Delete((Guid)addSpaceship.Id);
+
+            //kontroll
+            Assert.Equal(addSpaceship.Id, deleteSpaceship.Id);
+        }
+
+        private SpaceshipDto MockSpaceshipData(bool isOneOrTwo)
+        {
+            if
+            SpaceshipDto dto = new SpaceshipDto()
+            {
+                Name = "X AE a L 12",
+                ShipType = "lendav taldrik",
+                Crew = 666,
+                EnginePower = 69,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+        }
     }
 }
